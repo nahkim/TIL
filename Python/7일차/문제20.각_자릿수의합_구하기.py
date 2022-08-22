7569f0038fa39ac93889062291405900071058d3
nahkim <nahkim@student.42seoul.kr>
2022-08-23 00:29:14 +0900

    :memo: Add other method 220719
    다른 방법 주석 추가

--- Python/7일차/문제20.각_자릿수의합_구하기.py
+++ Python/7일차/문제20.각_자릿수의합_구하기.py
@@ -1,7 +1,11 @@
 number = int(input())
 res = 0
 
-while number != 0:
+while number != 0:  # while number:
     res += number % 10
     number //= 10
+
+    # 방법 2
+    # number, remainder = divmod(number, 10)
+    # res += remainder
 print(res)
--- Python/7일차/문제21.숫자_뒤집기.py
+++ Python/7일차/문제21.숫자_뒤집기.py
@@ -10,3 +10,11 @@ num_len = len(num_list)
 for i in range(len(num_list)):
     res += (10**i * num_list[num_len - i - 1])
 print(res)
+
+# 방법 2
+# res = 0
+# while num:
+#     res *= 10
+#     res += num % 10
+#     num //= 10
+# print(res)
